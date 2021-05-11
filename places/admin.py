2c1f0a6ada912dc68925f52885d2c6ec5aefb366
Aleksandr Karakchiev <realrushen@gmail.com>
2021-05-11 22:54:17 +0300

    add Place and MapPoint models

--- places/admin.py
+++ places/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
 
-# Register your models here.
+from places.models import Place, MapPoint
+
+
+class PlaceInline(admin.StackedInline):
+    model = Place
+    extra = 1
+
+
+@admin.register(Place)
+class PlaceAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(MapPoint)
+class MapPointAdmin(admin.ModelAdmin):
+    inlines = [
+        PlaceInline
+    ]
--- places/models.py
+++ places/models.py
@@ -1,3 +1,35 @@
 from django.db import models
 
-# Create your models here.
+class Place(models.Model):
+    """
+    Модель интересного места на карте
+    """
+    title = models.CharField('Название места', max_length=50, db_index=True)
+    description_short = models.CharField('Короткое описание', max_length=255)
+    description_long = models.TextField('Подробное описание')
+    coordinates = models.ForeignKey('MapPoint', related_name='places', on_delete=models.PROTECT,
+                                    verbose_name='координаты места')
+
+    class Meta:
+        verbose_name = 'Интересное место'
+        verbose_name_plural = 'Интересные места'
+
+    def __str__(self):
+        return f'{self.title} {self.coordinates}'
+
+
+class MapPoint(models.Model):
+    """Модель координат точки на карте"""
+    longitude = models.DecimalField('Долгота', max_digits=16, decimal_places=14)
+    latitude = models.DecimalField('Широта', max_digits=16, decimal_places=14)
+
+    class Meta:
+        verbose_name = 'Точка на карте'
+        verbose_name_plural = 'Точки на карте'
+        unique_together = [['longitude', 'latitude']]
+        index_together = [['longitude', 'latitude']]
+
+    def __str__(self):
+        return f'({self.longitude}, {self.latitude})'
+
+
