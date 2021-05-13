27a88e721dca408d4753597736a48e1b9025e37e
Aleksandr Karakchiev <realrushen@gmail.com>
2021-05-13 21:45:31 +0300

    Move data from template to view

--- places/views.py
+++ places/views.py
@@ -1,7 +1,71 @@
+import json
+from pprint import pprint
+
 from django.shortcuts import render
 
 
 # Create your views here.
 
 def index(request):
-    return render(request, template_name='places/index.html')
+    """
+    {
+      "type": "FeatureCollection",
+      "features": [
+        {
+          "type": "Feature",
+          "geometry": {
+            "type": "Point",
+            "coordinates": [37.62, 55.793676]
+          },
+          "properties": {
+            "title": "«Легенды Москвы",
+            "placeId": "moscow_legends",
+            "detailsUrl": "./static/places/moscow_legends.json"
+          }
+        },
+        {
+          "type": "Feature",
+          "geometry": {
+            "type": "Point",
+            "coordinates": [37.64, 55.753676]
+          },
+          "properties": {
+            "title": "Крыши24.рф",
+            "placeId": "roofs24",
+            "detailsUrl": "./static/places/roofs24.json"
+          }
+        }
+      ]
+    }
+    """
+    json_data = {
+      "type": "FeatureCollection",
+      "features": [
+        {
+          "type": "Feature",
+          "geometry": {
+            "type": "Point",
+            "coordinates": [37.62, 55.793676]
+          },
+          "properties": {
+            "title": "«Легенды Москвы",
+            "placeId": "moscow_legends",
+            "detailsUrl": "./static/places/moscow_legends.json"
+          }
+        },
+        {
+          "type": "Feature",
+          "geometry": {
+            "type": "Point",
+            "coordinates": [37.64, 55.753676]
+          },
+          "properties": {
+            "title": "Крыши24.рф",
+            "placeId": "roofs24",
+            "detailsUrl": "./static/places/roofs24.json"
+          }
+        }
+      ]
+    }
+
+    return render(request, context={'geo_data': json_data}, template_name='places/index.html')
