72df678d9ede0d3a912475bd4e25370f6fd8e269
Aleksandr Karakchiev <realrushen@gmail.com>
2021-05-22 00:41:44 +0300

    removed logging setting from production config

--- where_to_go/settings/production.py
+++ where_to_go/settings/production.py
@@ -14,21 +14,3 @@ ALLOWED_HOSTS = env.list('ALLOWED_HOSTS')
 CSRF_COOKIE_SECURE = True
 SESSION_COOKIE_SECURE = True
 
-LOGGING = {
-    'version': 1,
-    'disable_existing_loggers': False,
-    'handlers': {
-        'file': {
-            'level': 'DEBUG',
-            'class': 'logging.FileHandler',
-            'filename': BASE_DIR / 'logs/debug.log',
-        },
-    },
-    'loggers': {
-        'django': {
-            'handlers': ['file'],
-            'level': 'DEBUG',
-            'propagate': True,
-        },
-    },
-}
