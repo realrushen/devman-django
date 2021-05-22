a38a57518771c82a6903a66a9bc94dc8d005b226
Aleksandr Karakchiev <realrushen@gmail.com>
2021-05-22 10:05:02 +0300

    DATABASES setting changed

--- where_to_go/settings/common.py
+++ where_to_go/settings/common.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent.parent
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': 'db.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
